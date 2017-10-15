c8131e85eb9c5195e63f9882a44d4c4f545d3dda
fawad <you@example.com>
2017-10-16 02:50:11 +0530

    first commit

---
+++ wik_view.js
@@ -0,0 +1,27 @@
+$(document).ready(function()
+ {
+  $('#searchButton').click(function()
+ {
+  var searchTerm=  $('#searchTerm').val();
+  var url="https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm
+  +"&format=json&callback=?";
+    
+   $.ajax({
+        type:"GET",
+        url:url,
+        async:false,
+        dataType:"json",
+        success: function(data){
+        for(var i=0; i<data[1].length; i++)
+          {
+        $('#output').prepend("<li><a href="+data[3][i]+">"+data[1][i]+"</a><p>"+data[2][i]+"</p></li>");
+          }
+        },
+                   
+       error: function(errorMessage){
+         alert("Error");
+         }  
+   }); 
+    
+  });
+});
